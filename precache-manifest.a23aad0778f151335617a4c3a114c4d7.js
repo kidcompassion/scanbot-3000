9834eb709591f60177f30151002fe4369ae7c371
kidcompassion <sallypoulsen@gmail.com>
2020-02-25 16:42:58 -0700

    Updates

---
+++ precache-manifest.a23aad0778f151335617a4c3a114c4d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9fc5f447b4f41aac9735cbf28f1156",
+    "url": "/scanbot-3000/index.html"
+  },
+  {
+    "revision": "2503141ab572775e6e15",
+    "url": "/scanbot-3000/static/css/main.1876df9a.chunk.css"
+  },
+  {
+    "revision": "ede28127dc3e935a3125",
+    "url": "/scanbot-3000/static/js/2.90c51c10.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/scanbot-3000/static/js/2.90c51c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2503141ab572775e6e15",
+    "url": "/scanbot-3000/static/js/main.6c9e278d.chunk.js"
+  },
+  {
+    "revision": "be43aeb704fd648732fc",
+    "url": "/scanbot-3000/static/js/runtime-main.0ce8e870.js"
+  }
+]);
